eea311389fafa58694e3366e673904315430a6a1
rowan <rowan.pitt@btinternet.com>
2022-03-29 08:53:57 +0100

    Added email,name,camera validation

--- insert.php
+++ insert.php
@@ -13,9 +13,50 @@ if($conn === false){
 }
 		
 // Taking all values from the form data(input)
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$camera = $_REQUEST['camera'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$camera = $_POST['camera'];
+
+// Santitize name
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$inputs['name'] = $name;
+
+if ($name) {
+	$name = trim($name);
+	if ($name === '') {  
+		$errors['name'] = NAME_REQUIRED;
+	}
+	} else {
+		$errors['name'] = NAME_REQUIRED;
+}
+
+// Santitize email 
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$inputs['email'] = $email;
+
+if ($email) {
+	$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+	if ($email === false) {  
+		$errors['email'] = EMAIL_INVALID;
+	}
+	} else {
+		$errors['email'] = CAMERA_REQUIRED;
+}
+
+// Santitize camera
+$camera = filter_input(INPUT_POST, 'camera', FILTER_SANITIZE_STRING);
+$inputs['camera'] = $camera;
+
+if ($camera) {
+	$camera = trim($camera);
+	if ($camera === '') {  
+		$errors['camera'] = CAMERA_REQUIRED;
+	}
+	} else {
+		$errors['camera'] = CAMERA_REQUIRED;
+}
+
+if (count($errors) === 0 ) {
 
 // Performing insert query execution
 $sql = "INSERT INTO sell_your_camera (name_id,name,email,camera) VALUES (NULL,'$name',
@@ -32,6 +73,11 @@ if(mysqli_query($conn, $sql)){
 mysqli_close($conn);
 
 mail($admin_mail, 'Peterwalnes.com', 'Thank you for registering with us');
+
+} else { 
+	die;
+}
+
 
 ?>
 
