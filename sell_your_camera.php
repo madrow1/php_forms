e9489a809992e3de545dd66562e790419ed2f83b
rowan <rowan.pitt@btinternet.com>
2022-03-31 09:04:25 +0100

    added more text to sell_your_camera

--- sell_your_camera.php
+++ sell_your_camera.php
@@ -14,7 +14,8 @@
   
         <form action="insert.php" method="post">
               
-              
+        <h2 class="subtitle">Do you have any camera equipment you might want to sell? Please use this online form to tell us what you have. We will get back to you with our buying price.</h2> 
+        <h2 class="subtitle">Alternatively just call 01736 719461 for a quote.</h2>             
 <p>
                 <label for="name">Name:</label>
                 <input type="text" name="name" id="name">
